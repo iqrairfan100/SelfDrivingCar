7de98718f51afcb72fefddda9d9055704749eb84
iqrairfan100 <iqrairfan100@gmail.com>
2024-08-21 19:52:50 -0700

    roads

--- car.js
+++ car.js
@@ -7,7 +7,7 @@ class Car{
 
         this.speed = 0;
         this.acceleration = 0.2;
-        this.maxSpeed = 3;
+        this.maxSpeed = 10;
         this.friction = 0.05;
         this.angle=0;
 
@@ -15,43 +15,48 @@ class Car{
     }
 
     update(){
+        this.#move();
+
+    }
+
+    #move(){
         if(this.controls.forward){
-           this.speed += this.acceleration;
-        }
-        if(this.controls.reverse){
-            this.speed -= this.acceleration;
-        }
-        if(this.speed>this.maxSpeed){
-            this.speed = this.maxSpeed;
-        }
-        if(this.speed<-this.maxSpeed/2){
-            this.speed=-this.maxSpeed/2;
-        }
-
-        if(this.speed>0){
-            this.speed -= this.friction;
-        }
-        if(this.speed<0){
-            this.speed += this.friction;
-        }
-        if(Math.abs(this.speed)<this.friction){
-            this.speed = 0;
-        }
-
-        if(this.speed!=0){
-            const flip = this.speed>0?1:-1;
-            if(this.controls.left){
-                this.angle+=0.03*flip;
-            }
-            if(this.controls.right){
-                this.angle-=0.03*flip;
-            }
-        }
-        
-
-        this.x-=Math.sin(this.angle)*this.speed;
-        this.y-=Math.cos(this.angle)*this.speed;
-
+            this.speed += this.acceleration;
+         }
+         if(this.controls.reverse){
+             this.speed -= this.acceleration;
+         }
+         if(this.speed>this.maxSpeed){
+             this.speed = this.maxSpeed;
+         }
+         if(this.speed<-this.maxSpeed/2){
+             this.speed=-this.maxSpeed/2;
+         }
+ 
+         if(this.speed>0){
+             this.speed -= this.friction;
+         }
+         if(this.speed<0){
+             this.speed += this.friction;
+         }
+         if(Math.abs(this.speed)<this.friction){
+             this.speed = 0;
+         }
+ 
+         if(this.speed!=0){
+             const flip = this.speed>0?1:-1;
+             if(this.controls.left){
+                 this.angle+=0.03*flip;
+             }
+             if(this.controls.right){
+                 this.angle-=0.03*flip;
+             }
+         }
+         
+ 
+         this.x-=Math.sin(this.angle)*this.speed;
+         this.y-=Math.cos(this.angle)*this.speed;
+ 
     }
 
     draw(ctx){
--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const canvas=document.getElementById("myCar");
 canvas.height=window.innerHeight;
-canvas.width=200;
+canvas.width=400;
 
 const ctx = canvas.getContext("2d");
+const road=new Road(canvas.width/2, canvas.width *0.9);
 const car = new Car(100,100,30,50);
 
 animate();
@@ -10,6 +11,7 @@ animate();
 function animate(){
     car.update();
     canvas.height = window.innerHeight;
+    road.draw(ctx);
     car.draw(ctx);
     requestAnimationFrame(animate);
 }
